99cd63dc62690571572dddf5ccc7dfcd21db9627
1NF1N18Y <1nf1n19y@gmail.com>
2024-04-02 19:26:48 +0200

    fix X10 activation + logging

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -4,54 +4,63 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <primitives/block.h>
-
+#include <logging.h>
 #include <crypto/common.h>
 #include <hash.h>
 #include <streams.h>
 #include <tinyformat.h>
 #include <utilstrencodings.h>
 
-static const uint32_t MAINNET_ALGOCHANGEX10 = 1712079120;
-
+static const uint32_t MAINNET_X11 = 1710107402;
+static const uint32_t MAINNET_ALGOCHANGEX10 = 1710107302;
 
 //TODO figure out whats best for activating X10
-// uint256 CBlockHeader::GetHash() const
-// {
-//     if (nTime < MAINNET_X11ACTIVATIONTIME) {
-//         std::vector<unsigned char> vch(80);
-//         CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
-//         ss << *this;
-//         return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
-//     } else if (nTime < MAINNET_X10ACTIVATIONTIME) {
-//         std::vector<unsigned char> vch(80);
-//         CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
-//         ss << *this;
-//         return HashX10((const char*)vch.data(), (const char*)vch.data() + vch.size());
-//     }
-//     std::vector<unsigned char> vch(80);
-//     CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
-//     ss << *this;
-//     return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
-// }
-
-
 uint256 CBlockHeader::GetHash() const
 {
-    if (nTime < MAINNET_ALGOCHANGEX10) {
-        uint32_t nTimeToUse = MAINNET_ALGOCHANGEX10;
-        if (nTime >= nTimeToUse) {
-            std::vector<unsigned char> vch(80);
-            CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
-            ss << *this;
-            return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
-        }
-
+    if (nTime < MAINNET_X11) {
         std::vector<unsigned char> vch(80);
         CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
         ss << *this;
+        LogPrintf("Using x11 algo \n");
+        return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
+    } else if (nTime >= MAINNET_ALGOCHANGEX10) {
+        std::vector<unsigned char> vch(80);
+        CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
+        ss << *this;
+        LogPrintf("Using x10 algo \n");
         return HashX10((const char*)vch.data(), (const char*)vch.data() + vch.size());
+    } else {
+        std::vector<unsigned char> vch(80);
+        CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
+        ss << *this;
+        LogPrintf("Using x11 algo \n");
+        return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
     }
 }
+
+
+
+// uint256 CBlockHeader::GetHash() const
+// {
+//     if (nTime < MAINNET_ALGOCHANGEX10) {
+//         uint32_t nTimeToUse = MAINNET_X11;
+//         if (nTime >= nTimeToUse) {
+//             std::vector<unsigned char> vch(80);
+//             CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
+//             ss << *this;
+//             LogPrintf("Starting x10 \n");
+//             return HashX10((const char*)vch.data(), (const char*)vch.data() + vch.size());
+//         }
+
+//         std::vector<unsigned char> vch(80);
+//         CVectorWriter ss(SER_GETHASH, PROTOCOL_VERSION, vch, 0);
+//         ss << *this;
+//         LogPrintf("Starting x11 \n");
+//         return HashX11((const char*)vch.data(), (const char*)vch.data() + vch.size());
+//     }
+
+// }
+
 
 
 std::string CBlock::ToString() const
